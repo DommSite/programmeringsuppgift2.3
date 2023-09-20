f0a45f062e0b653db35ac8e8a57ee8275465dd69
DommSite <johan.johansson06@live.se>
2023-09-20 10:11:38 +0200

    Add project files.

---
+++ programmeringsuppgift2.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+namespace övning2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många dagar ska du köra din hyrda bil?");
+            int dagar = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur långt hade du tänkt att köra? (i km)");
+            float köravstånd = float.Parse(Console.ReadLine());
+
+            float kostnad = (float)dagar * 500 + köravstånd * 1 - 200;
+
+            Console.WriteLine("Din totala kostnad kommer vara " + kostnad + "kr.");
+
+
+            Console.ReadKey();
+        }
+    }
+}
